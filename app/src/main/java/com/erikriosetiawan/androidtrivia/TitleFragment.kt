985118f469d4b235177f6f0bac71a071e615f3b0
erikrios <erikriosetiawan49@gmail.com>
2020-01-01 17:38:01 +0700

    Rename fragment_title.xml

---
+++ app/src/main/java/com/erikriosetiawan/androidtrivia/TitleFragment.kt
@@ -0,0 +1,23 @@
+package com.erikriosetiawan.androidtrivia
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class TitleFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = DataBindingUtil.inflate<FragmentTitleBinding>()
+    }
+}

999e4524471fb8c2038d845ad134f8fe4acfd83d
erikrios <erikriosetiawan49@gmail.com>
2020-01-01 17:40:45 +0700

    Add a fragment class

--- app/src/main/java/com/erikriosetiawan/androidtrivia/TitleFragment.kt
+++ app/src/main/java/com/erikriosetiawan/androidtrivia/TitleFragment.kt
@@ -2,12 +2,12 @@ package com.erikriosetiawan.androidtrivia
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.erikriosetiawan.androidtrivia.databinding.FragmentTitleBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -18,6 +18,13 @@ class TitleFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = DataBindingUtil.inflate<FragmentTitleBinding>()
+        val binding =
+            DataBindingUtil.inflate<FragmentTitleBinding>(
+                inflater,
+                R.layout.fragment_title,
+                container,
+                false
+            )
+        return binding.root
     }
 }

1423bfb2b5dbfc7008f2221539d0636f8b8f4583
erikrios <erikriosetiawan49@gmail.com>
2020-01-02 20:38:52 +0700

    Add more navigation actions and add onClick handlers

--- app/src/main/java/com/erikriosetiawan/androidtrivia/GameWonFragment.kt
+++ app/src/main/java/com/erikriosetiawan/androidtrivia/GameWonFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.erikriosetiawan.androidtrivia.databinding.FragmentGameWonBinding
 
 class GameWonFragment : Fragment() {
@@ -19,6 +20,9 @@ class GameWonFragment : Fragment() {
         // Inflate the layout for this fragment
         val binding: FragmentGameWonBinding =
             DataBindingUtil.inflate(inflater, R.layout.fragment_game_won, container, false)
+        binding.nextMatchButton.setOnClickListener { view: View ->
+            view.findNavController().navigate(R.id.action_gameWonFragment_to_gameFragment)
+        }
         return binding.root
     }
 }

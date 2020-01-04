5f57ebbeb50e4f06922288dfcb121b5078e036ce
erikrios <erikriosetiawan49@gmail.com>
2020-01-04 16:32:48 +0700

    Pass the arguments

--- app/src/main/java/com/erikriosetiawan/androidtrivia/GameFragment.kt
+++ app/src/main/java/com/erikriosetiawan/androidtrivia/GameFragment.kt
@@ -122,10 +122,16 @@ class GameFragment : Fragment() {
                         setQuestion()
                         binding.invalidateAll()
                     } else {
-                        view.findNavController().navigate(GameFragmentDirections.actionGameFragmentToGameWonFragment())
+                        view.findNavController().navigate(
+                            GameFragmentDirections.actionGameFragmentToGameWonFragment(
+                                numQuestions,
+                                questionIndex
+                            )
+                        )
                     }
                 } else {
-                    view.findNavController().navigate(GameFragmentDirections.actionGameFragmentToGameOverFragment())
+                    view.findNavController()
+                        .navigate(GameFragmentDirections.actionGameFragmentToGameOverFragment())
                 }
             }
         }
--- app/src/main/java/com/erikriosetiawan/androidtrivia/GameWonFragment.kt
+++ app/src/main/java/com/erikriosetiawan/androidtrivia/GameWonFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
@@ -23,6 +24,12 @@ class GameWonFragment : Fragment() {
         binding.nextMatchButton.setOnClickListener { view: View ->
             view.findNavController().navigate(R.id.action_gameWonFragment_to_gameFragment)
         }
+        val args = GameWonFragmentArgs.fromBundle(arguments!!)
+        Toast.makeText(
+            context,
+            "NumCorrect: ${args.numCorrect}, NumQuestions: ${args.numQuestions}",
+            Toast.LENGTH_LONG
+        ).show()
         return binding.root
     }
 }
